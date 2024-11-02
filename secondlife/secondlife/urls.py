dac8e7a59199346f525bacee11ed6d614b330a5e
DmitriyQW <varavkadmitrij@gmail.com>
2024-11-02 16:47:21 +0300

    Дата:02.11.2024
    Добавлено :
    1 Создал приложение serviceCenter
    2 Создал обрабочик ответов на переход по url

--- secondlife/secondlife/urls.py
+++ secondlife/secondlife/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from serviceCenter.views import index,pricelist
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index), #Главная страница google
+    path('pricelist/',pricelist),#127.0.0.1:8000/pricelist
 ]
