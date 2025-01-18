d9ccdd2bd0789fb4bdf631fa5c4a69eb98f6ed2c
DmitriyQW <varavkadmitrij@gmail.com>
2025-01-18 15:51:17 +0300

    Зарегистрировал модели для Администратора. Изменил стандартный вывод workerObject на id+Фио и т.п. Добавил в таблицу Feedback, Publications поле Date_feedback, date_public. Добавил авто заполнение даты текущей timezone.now.

--- secondlife/serviceCenter/admin.py
+++ secondlife/serviceCenter/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import (Worker,User,State_applic,Type_applic,
+                     Manufacturer_applic,Application,PriceList,
+                     Report,Feedbackcol_number,Feedback,Publications,Chat)
+
+# Регистрируем каждую модель
+admin.site.register(Worker)
+admin.site.register(User)
+admin.site.register(State_applic)
+admin.site.register(Type_applic)
+admin.site.register(Manufacturer_applic)
+admin.site.register(Application)
+admin.site.register(PriceList)
+admin.site.register(Report)
+admin.site.register(Feedbackcol_number)
+admin.site.register(Feedback)
+admin.site.register(Publications)
+admin.site.register(Chat)
--- secondlife/serviceCenter/models.py
+++ secondlife/serviceCenter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator #Валидация
+from django.utils import timezone
 from django.contrib.auth.hashers import make_password #Хеширования пароля
 # Create your models here.
 
@@ -15,7 +16,10 @@ class Worker(models.Model):
     address_worker = models.CharField(max_length=60)
     age_worker = models.PositiveSmallIntegerField(validators=
     [MaxValueValidator(100)]) #Валидация максимальный возврат 100 лет
-    dateregister_worker = models.DateField()
+    dateregister_worker = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.id_worker} ({self.fio_worker})" # Возвращение id и Фио мастера
 
 class User(models.Model):
     id_user = models.AutoField(primary_key=True)
@@ -29,19 +33,31 @@ class User(models.Model):
     address_user = models.CharField(max_length=60)
     age_user = models.PositiveSmallIntegerField(validators=
     [MaxValueValidator(100)]) #Валидация максимальный возврат 100 лет
-    dateregister_user = models.DateField()
+    dateregister_user = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.id_user}({self.fio_user})"
 
 class State_applic(models.Model):
     id_state = models.AutoField(primary_key=True)
     name_state = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"{self.id_state}({self.name_state})"
+
 class Type_applic(models.Model):
     id_type = models.AutoField(primary_key=True)
     name_type = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f"{self.id_type}({self.name_type})"
+
 class Manufacturer_applic(models.Model):
     id_manufacturer = models.AutoField(primary_key=True)
     name_manufacturer = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"{self.id_manufacturer}({self.name_manufacturer})"
 
 class Application(models.Model):
     id_application = models.AutoField(primary_key=True)
@@ -55,7 +71,7 @@ class Application(models.Model):
     model_applic = models.CharField(max_length=45)
     password_applic = models.CharField(max_length=45)
     other_applic = models.CharField(max_length=45)
-    date_applic = models.DateField()
+    date_applic = models.DateTimeField(default=timezone.now)
     #Null
     adresssamoviz_applic = models.CharField(max_length=45,blank=True,null=True)
     telmastersamoviz_applic = models.CharField(max_length=45,blank=True,null=True)
@@ -63,6 +79,8 @@ class Application(models.Model):
     verdict_applic = models.CharField(max_length=45,blank=True,null=True)
     descriptionprice_applic = models.CharField(max_length=45,blank=True,null=True)
 
+    def __str__(self):
+        return f"{self.id_application}({self.date_applic}({self.id_state_applic}({self.id_user_applic})({self.reason_applic})))"
 
 class PriceList(models.Model):
     id_service = models.AutoField(primary_key=True)
@@ -76,17 +94,27 @@ class Report(models.Model):
     ip = models.CharField(max_length=45)
     device = models.CharField(max_length=45)
     action = models.CharField(max_length=45)
-    date_action = models.DateTimeField()
+    date_action = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.id_Report}({self.login}({self.action}({self.date_action})))"
 
 class Feedbackcol_number(models.Model):
     id_feedbackcol_number = models.AutoField(primary_key=True)
     number = models.CharField(max_length=2) #!!!
+
+    def __str__(self):
+        return f"{self.id_feedbackcol_number}({self.number})"
 
 class Feedback(models.Model):
     id_feedback = models.AutoField(primary_key=True)
     id_user = models.ForeignKey(User,on_delete=models.PROTECT)
     id_feedbackcol_number = models.ForeignKey(Feedbackcol_number,on_delete=models.PROTECT)
     description_service = models.CharField(max_length=45)
+    date_feedback = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.id_feedback}({self.date_feedback})({self.id_user})({self.date_feedback})({self.id_feedbackcol_number})"
 
 class Publications(models.Model):
     id_publ = models.AutoField(primary_key=True)
@@ -94,10 +122,17 @@ class Publications(models.Model):
     photo_publ = models.CharField(max_length=45)
     description_publ = models.CharField(max_length=45)
     source_public = models.CharField(max_length=45)
+    date_public = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.id_publ}({self.date_public})({self.description_publ})({self.source_public})({self.id_worker_public})"
 
 class Chat(models.Model):
     id_chat = models.AutoField(primary_key=True)
     master_chat = models.ForeignKey(Worker,on_delete=models.PROTECT)
     user_chat = models.ForeignKey(User,on_delete=models.PROTECT)
-    date_chat = models.DateTimeField()
+    date_chat = models.DateTimeField(default=timezone.now)
     message_chat = models.TextField()
+
+    def __str__(self):
+        return f"{self.id_chat}({self.date_chat})({self.user_chat})({self.master_chat})({self.master_chat})"
